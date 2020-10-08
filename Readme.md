26f7dde53496643e8f5b56f7a396d3f3a46d8d9b
Turtle <turtleletortuelab@gmail.com>
2020-10-08 12:36:33 -0400

    pocket2: Latest theme change from thoramund, add credit

--- Readme.md
+++ Readme.md
@@ -7,3 +7,4 @@ Based on https://github.com/mt8163/android_device_bq_aquaris_m8 and https://gith
 
 Thanks to @R0rt1z2 & @488315 for helping on this!
 Special thanks to Retroid for releasing kernel source code!
+Thanks to @thoramund(github.com/thoramund) for tweaking the 320x240 theme to work with Retroid Pocket 2
--- device.mk
+++ device.mk
@@ -3,6 +3,6 @@ TARGET_PREBUILT_KERNEL := device/retroid/pocket2/kernel
 
 PRODUCT_COPY_FILES += \
     $(LOCAL_PATH)/recovery/fstab.mt6580:root/fstab.mt6580 \
-    $(LOCAL_PATH)/recovery/ueventd.mt6580.rc:root/ueventd.mt6580.rc 
+    $(LOCAL_PATH)/recovery/ueventd.mt6580.rc:root/ueventd.mt6580.rc \
 
 $(call inherit-product, $(SRC_TARGET_DIR)/product/aosp_base.mk)
