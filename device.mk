67c0ce40a807d1371b3545a02d14e7d86f461406
Turtle <turtleletortuelab@gmail.com>
2020-10-14 21:31:04 -0400

    pocket2: Fix adb, mount psystem

--- device.mk
+++ device.mk
@@ -4,5 +4,6 @@ TARGET_PREBUILT_KERNEL := device/retroid/pocket2/kernel
 PRODUCT_COPY_FILES += \
     $(LOCAL_PATH)/recovery/fstab.mt6580:root/fstab.mt6580 \
     $(LOCAL_PATH)/recovery/ueventd.mt6580.rc:root/ueventd.mt6580.rc \
+    $(LOCAL_PATH)/recovery/init.recovery.mt6580.rc:root/init.recovery.mt6580.rc \
 
 $(call inherit-product, $(SRC_TARGET_DIR)/product/aosp_base.mk)
